9d49df68acb837ac38cd1571f297d3f64e6aad36
Mazharul Islam <155420502+d-mazharul@users.noreply.github.com>
2024-01-06 10:18:52 +0600

    Update PostController.php

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,14 +9,14 @@ class PostController extends Controller
     public function __construct()
     {
         /**
-         * Permissions, role, & role_permission middleware must register in app/http/Kernal.php
+         * Permission, role, & role_or_permission middleware must register in app/http/Kernal.php
          *
          * check http kernel file
         */
-        $this->middleware('permissions:post-list|post-create|post-edit|post-delete', ['only' => ['index', 'show']]); // those permission are set in permissions table
-        $this->middleware('permissions:post-create', ['only' => ['store']]); // usually use Seeder to input permissions
-        $this->middleware('permissions:post-edit', ['only' => ['update']]); // add new permission for you controller and use here in consturctor
-        $this->middleware('permissions:post-delete', ['only' => ['destroy']]);
+        $this->middleware('permission:post-list|post-create|post-edit|post-delete', ['only' => ['index', 'show']]); // those permission are set in permissions table
+        $this->middleware('permission:post-create', ['only' => ['store']]); // usually use Seeder to input permissions
+        $this->middleware('permission:post-edit', ['only' => ['update']]); // add new permission for you controller and use here in consturctor
+        $this->middleware('permission:post-delete', ['only' => ['destroy']]);
     }
 
     public function index()
